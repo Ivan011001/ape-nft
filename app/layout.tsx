2daed3333eae8dcd57531513751994a4b7769eb4
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-18 17:38:41 +0200

    init: added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const rightGrotesk = localFont({ src: "../public/RightGrotesk/font.otf" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={rightGrotesk.className}>{children}</body>
     </html>
   );
 }
