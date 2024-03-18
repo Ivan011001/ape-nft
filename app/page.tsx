7f5538ae615450d3ff228624c30ee0309730ee30
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-18 19:11:20 +0200

    fix: fonts placement

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 const HomePage = () => {
   return (
     <div>
-      <p>Hello Right Grotesk</p>
-      <p className="text-xl font-biroScript">Hello Biro Script</p>
-      <p className="text-xl font-biroScriptUS">Hello Biro ScriptUS</p>
-      <p className="text-xl font-messinaSans">Hello Messina Sans regular</p>
-      <p className="text-xl font-messinaSans font-semibold">
+      <div className=" text-[188px] leading-[188px]">YACHT APES</div>
+
+      <p className="font-biroScript">Hello Biro Script</p>
+      <p className="font-biroScriptUS">Hello Biro ScriptUS</p>
+      <p className="font-messinaSans">Hello Messina Sans regular</p>
+      <p className="font-messinaSans font-semibold">
         Hello Messina Sans semibold
       </p>
     </div>
