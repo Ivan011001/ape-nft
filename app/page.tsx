ba45bad5bf8c09fae9b8bfdcc136bd43521383a8
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-18 19:05:08 +0200

    fix: simplified font usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 const HomePage = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <p>Hello Right Grotesk</p>
+      <p className="text-xl font-biroScript">Hello Biro Script</p>
+      <p className="text-xl font-biroScriptUS">Hello Biro ScriptUS</p>
+      <p className="text-xl font-messinaSans">Hello Messina Sans regular</p>
+      <p className="text-xl font-messinaSans font-semibold">
+        Hello Messina Sans semibold
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ utils/customFonts.ts
@@ -0,0 +1,37 @@
+import localFont from "next/font/local";
+
+const biroScript = localFont({
+  src: [
+    {
+      path: "../public/fonts/BiroScript/font-bold.woff2",
+    },
+  ],
+  variable: "--font-biro-script",
+});
+
+const biroScriptUS = localFont({
+  src: [
+    {
+      path: "../public/fonts/BiroScript/font-us-bold.woff2",
+    },
+  ],
+  variable: "--font-biro-script-us",
+});
+
+const messinaSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/MessinaSans/font-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/MessinaSans/font-semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-messina-sans",
+});
+
+export { biroScript, biroScriptUS, messinaSans };
