8b0710a32ea70fcd27c11f554876887c80475376
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-20 18:50:26 +0200

    fix: mind map hover

--- components/mind-map.tsx
+++ components/mind-map.tsx
@@ -49,7 +49,7 @@ const MindMap = () => {
           </div>
         ))}
 
-        <button className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-rose-500 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between items-start text-start hover:bg-stone-900 transition-all duration-300">
+        <button className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-rose-500 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between items-start text-start">
           <svg className="w-12 h-12 lg:w-16 lg:h-16 self-end">
             <use xlinkHref="/icons/sprite.svg#icon-up-left"></use>
           </svg>
