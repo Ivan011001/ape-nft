c4d06cd0253f61823367774e255ebf181eea43ee
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-20 19:19:37 +0200

    feat: art list

--- components/arts.tsx
+++ components/arts.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+
+import Pagination from "./ui/pagination";
 import Section from "./ui/section";
 import Title from "./ui/title";
 
@@ -6,6 +9,22 @@ const Arts = () => {
     <Section>
       <div className="mb-6 md:mb-10 lg:mb-20">
         <Title>Collection</Title>
+      </div>
+
+      <div className="flex flex-col items-center gap-y-6 lg:gap-y-12">
+        <ul className="flex items-center gap-x-6">
+          <li>
+            <Image
+              src="/images/art/ape-2.jpg"
+              alt="Collection Ape"
+              width={284}
+              height={336}
+              className="w-[216px] h-[256px] md:w-[284px] md:h-[336px] lg:w-[240px] lg:h-[280px] rounded-xl md:rounded-2xl lg:rounded-3xl"
+            />
+          </li>
+        </ul>
+
+        <Pagination />
       </div>
     </Section>
   );
--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,10 +1,10 @@
 const Pagination = () => {
   return (
     <div className="flex items-center justify-center gap-12 mt-6">
-      <button className="text-white text-2xl font-normal font-biroScript leading-normal">
+      <button className="text-white text-2xl font-normal font-biroScript leading-normal hover:text-rose-500 transition-all duration-300">
         Prev
       </button>
-      <button className="text-white text-2xl font-normal font-biroScript leading-normal">
+      <button className="text-white text-2xl font-normal font-biroScript leading-normal hover:text-rose-500 transition-all duration-300">
         Next
       </button>
     </div>
