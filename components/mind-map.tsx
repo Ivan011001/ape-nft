b6d251d284c4cb8ff73ebbf825bb0285c9924613
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-20 23:33:31 +0200

    feat: created constant variables

--- components/mind-map.tsx
+++ components/mind-map.tsx
@@ -1,28 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
 import Pagination from "./ui/pagination";
 import Section from "./ui/section";
 import Title from "./ui/title";
 
-const CARDS = [
-  {
-    title: "Yape Drop",
-    description:
-      "All owners of APE NFTs and all future collections will receive a percentage of sales based on the number of NFTs they own",
-  },
-
-  {
-    title: "New Collection",
-    description:
-      "Launch of the 2nd YACHT Collection Releasing the first version of the Ape Slam Game",
-  },
-
-  {
-    title: "Token",
-    description:
-      "Launch your own token, the proceeds of which will go to a global fund to LAUNCH YACHT CLUB AND PROMOTE it",
-  },
-];
+import { MIND_MAP_ITEMS } from "@/constants";
 
 const MindMap = () => {
+  const isTablet = useMediaQuery("(min-width: 768px)");
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const prevPage = () => {
+    setCurrentPage((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+
+  const nextPage = () => {
+    setCurrentPage((prev) =>
+      prev === MIND_MAP_ITEMS.length ? MIND_MAP_ITEMS.length : prev + 1
+    );
+  };
+
+  const visibleCard =
+    currentPage === MIND_MAP_ITEMS.length ? null : MIND_MAP_ITEMS[currentPage];
+
   return (
     <Section>
       <div className="mb-6 md:mb-10 lg:mb-20">
@@ -33,38 +36,39 @@ const MindMap = () => {
         className="grid grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(284px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(504px,1fr))] gap-x-6 gap-y-6"
         style={{ gridAutoFlow: "row" }}
       >
-        {CARDS.map((card, index) => (
-          <div
-            key={index}
-            className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-stone-900 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between"
-          >
-            <div className="md:flex justify-end">
-              <p className="md:w-32 lg:w-[256px] text-white text-xs lg:text-2xl font-normal font-messinaSans uppercase leading-[14px] lg:leading-[29px] float-right">
-                {card.description}
-              </p>
+        <>
+          {visibleCard && (
+            <div className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-stone-900 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between">
+              <div className="md:flex justify-end">
+                <p className="md:w-32 lg:w-[256px] text-white text-xs lg:text-2xl font-normal font-messinaSans uppercase leading-[14px] lg:leading-[29px] float-right">
+                  {visibleCard.description}
+                </p>
+              </div>
+
+              <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
+                {visibleCard.title}
+              </h3>
             </div>
+          )}
 
-            <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
-              {card.title}
-            </h3>
-          </div>
-        ))}
+          {currentPage === MIND_MAP_ITEMS.length && (
+            <button className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-rose-500 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between items-start text-start">
+              <svg className="w-12 h-12 lg:w-16 lg:h-16 self-end">
+                <use xlinkHref="/icons/sprite.svg#icon-up-left"></use>
+              </svg>
 
-        <button className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-rose-500 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between items-start text-start">
-          <svg className="w-12 h-12 lg:w-16 lg:h-16 self-end">
-            <use xlinkHref="/icons/sprite.svg#icon-up-left"></use>
-          </svg>
-
-          <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
-            Learn <br className="hidden lg:block" />
-            more <br className="hidden lg:block" />
-            in mind map
-          </h3>
-        </button>
+              <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
+                Learn <br className="hidden lg:block" />
+                more <br className="hidden lg:block" />
+                in mind map
+              </h3>
+            </button>
+          )}
+        </>
       </div>
 
       <div className="md:hidden">
-        <Pagination />
+        <Pagination prev={prevPage} next={nextPage} />
       </div>
     </Section>
   );
--- components/ui/socials.tsx
+++ components/ui/socials.tsx
@@ -1,31 +1,16 @@
-const SOCIALS = [
-  {
-    href: "",
-    icon: <use xlinkHref="/icons/sprite.svg#icon-discord"></use>,
-  },
-
-  {
-    href: "",
-    icon: <use xlinkHref="/icons/sprite.svg#icon-ship"></use>,
-  },
-
-  {
-    href: "",
-    icon: <use xlinkHref="/icons/sprite.svg#icon-twitter"></use>,
-  },
-];
+import { SOCIAL_ITEMS } from "@/constants";
 
 const Socials = () => {
   return (
     <ul className="flex flex-col gap-y-2 lg:gap-y-4">
-      {SOCIALS.map((social, index) => (
+      {SOCIAL_ITEMS.map((social, index) => (
         <li
           key={index}
           className="h-12 w-12 lg:h-20 lg:w-20 bg-stone-900 bg-opacity-10 rounded-lg md:rounded-[12px] backdrop-blur-md"
         >
           <button className="group w-full h-full flex justify-center items-center text-stone-900 text-xs font-semibold font-messinaSans leading-[14px]">
             <svg className="h-4 w-4 md:w-6 md:h-6 fill-stone-900 group-hover:fill-white transition-all duration-300">
-              {social.icon}
+              <use xlinkHref={`/icons/sprite.svg#icon-${social.icon}`}></use>
             </svg>
           </button>
         </li>
