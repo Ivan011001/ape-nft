ada7e355839d9c83359b0620e49efb020624cd11
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-21 11:09:45 +0200

    fix: mind map

--- components/mind-map.tsx
+++ components/mind-map.tsx
@@ -36,22 +36,25 @@ const MindMap = () => {
         className="grid grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(284px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(504px,1fr))] gap-x-6 gap-y-6"
         style={{ gridAutoFlow: "row" }}
       >
-        <>
-          {visibleCard && (
-            <div className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-stone-900 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between">
-              <div className="md:flex justify-end">
-                <p className="md:w-32 lg:w-[256px] text-white text-xs lg:text-2xl font-normal font-messinaSans uppercase leading-[14px] lg:leading-[29px] float-right">
-                  {visibleCard.description}
-                </p>
+        {isTablet ? (
+          <>
+            {MIND_MAP_ITEMS.map((item, index) => (
+              <div
+                key={index}
+                className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-stone-900 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between"
+              >
+                <div className="md:flex justify-end">
+                  <p className="md:w-32 lg:w-[256px] text-white text-xs lg:text-2xl font-normal font-messinaSans uppercase leading-[14px] lg:leading-[29px] float-right">
+                    {item.description}
+                  </p>
+                </div>
+
+                <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
+                  {item.title}
+                </h3>
               </div>
+            ))}
 
-              <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
-                {visibleCard.title}
-              </h3>
-            </div>
-          )}
-
-          {currentPage === MIND_MAP_ITEMS.length && (
             <button className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-rose-500 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between items-start text-start">
               <svg className="w-12 h-12 lg:w-16 lg:h-16 self-end">
                 <use xlinkHref="/icons/sprite.svg#icon-up-left"></use>
@@ -63,8 +66,38 @@ const MindMap = () => {
                 in mind map
               </h3>
             </button>
-          )}
-        </>
+          </>
+        ) : (
+          <>
+            {visibleCard && (
+              <div className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-stone-900 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between">
+                <div className="md:flex justify-end">
+                  <p className="md:w-32 lg:w-[256px] text-white text-xs lg:text-2xl font-normal font-messinaSans uppercase leading-[14px] lg:leading-[29px] float-right">
+                    {visibleCard.description}
+                  </p>
+                </div>
+
+                <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
+                  {visibleCard.title}
+                </h3>
+              </div>
+            )}
+
+            {currentPage === MIND_MAP_ITEMS.length && (
+              <button className="w-[216px] h-[242px] md:w-[284px] lg:w-[504px] lg:h-[480px] px-3 py-6 md:px-6 bg-rose-500 rounded-xl md:rounded-2xl lg:rounded-3xl flex flex-col justify-between items-start text-start">
+                <svg className="w-12 h-12 lg:w-16 lg:h-16 self-end">
+                  <use xlinkHref="/icons/sprite.svg#icon-up-left"></use>
+                </svg>
+
+                <h3 className="text-white text-[32px] lg:text-[64px] font-black uppercase leading-[32px] lg:leading-[64px]">
+                  Learn <br className="hidden lg:block" />
+                  more <br className="hidden lg:block" />
+                  in mind map
+                </h3>
+              </button>
+            )}
+          </>
+        )}
       </div>
 
       <div className="md:hidden">
