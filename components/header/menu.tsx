eaaf973da9a6d005828ce5e25624c7434505c811
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-22 17:58:51 +0200

    fix: mobile menu

--- components/header/menu.tsx
+++ components/header/menu.tsx
@@ -1,24 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
 import Link from "next/link";
 
 import Logo from "../ui/logo";
 import Socials from "./socials";
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import { MENU_ITEMS } from "@/constants";
 
 import { cn } from "@/lib/utils";
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
         <button
+          onClick={toggleIsOpen}
           className={cn(
             "h-12 w-12 lg:h-20 lg:w-20 bg-stone-900 bg-opacity-10 rounded-l-lg rounded-r-lg lg:rounded-l-[12px] lg:rounded-r-[12px] backdrop-blur-md flex justify-center items-center text-stone-900 text-xs lg:text-base font-semibold font-messinaSans leading-[14px] lg:leading-[19px] uppercase hover:underline hover:text-white transition-all duration-300"
           )}
@@ -31,18 +37,18 @@ const Menu = () => {
         <div>
           <div className="flex flex-col gap-y-2 lg:gap-y-4 mb-4 justify-between">
             <div className="w-full flex justify-between items-center">
-              <SheetClose asChild>
+              <button onClick={toggleIsOpen} className="p-0">
                 <Logo light />
-              </SheetClose>
-              <SheetClose asChild>
-                <button
-                  className={cn(
-                    "h-12 w-12 lg:h-20 lg:w-20 bg-white bg-opacity-10 rounded-lg lg:rounded-[12px] backdrop-blur-md flex justify-center items-center text-white text-xs lg:text-base font-semibold font-messinaSans leading-[14px] lg:leading-[19px] uppercase hover:underline hover:text-rose-500 transition-all duration-300"
-                  )}
-                >
-                  Close
-                </button>
-              </SheetClose>
+              </button>
+
+              <button
+                onClick={toggleIsOpen}
+                className={cn(
+                  "h-12 w-12 lg:h-20 lg:w-20 bg-white bg-opacity-10 rounded-lg lg:rounded-[12px] backdrop-blur-md flex justify-center items-center text-white text-xs lg:text-base font-semibold font-messinaSans leading-[14px] lg:leading-[19px] uppercase hover:underline hover:text-rose-500 transition-all duration-300"
+                )}
+              >
+                Close
+              </button>
             </div>
 
             <div className="self-end">
@@ -52,14 +58,14 @@ const Menu = () => {
 
           <nav className="flex flex-col gap-y-4 items-center">
             {MENU_ITEMS.map((item, index) => (
-              <SheetClose asChild key={index}>
-                <Link
-                  href={`#${item.href}`}
-                  className="text-white text-2xl font-semibold font-messinaSans leading-[29px] uppercase"
-                >
-                  {item.title}
-                </Link>
-              </SheetClose>
+              <Link
+                onClick={toggleIsOpen}
+                key={index}
+                href={`#${item.href}`}
+                className="text-white text-2xl font-semibold font-messinaSans leading-[29px] uppercase"
+              >
+                {item.title}
+              </Link>
             ))}
           </nav>
         </div>
